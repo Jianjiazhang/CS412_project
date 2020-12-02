a6664c43736affc4e3222b7ff01fde9e85d4d156
Qiyang Chen <38115801+woshicqy@users.noreply.github.com>
2020-12-02 03:51:54 -0600

    Add files via upload

--- opts.py
+++ opts.py
@@ -1,12 +1,9 @@
 import argparse
 import ast
-
-
 def get_opts():
-    parser = argparse.ArgumentParser(
-        description='CS412-sensor_data_mining_project')
+    parser = argparse.ArgumentParser(description='CS412-sensor_data_mining_project')
 
-    # Feature detection (requires tuning)
+    ## Feature detection (requires tuning)
     parser.add_argument('--data_path', type=str, default='./data/',
                         help='data path for storing data')
 
@@ -16,11 +13,7 @@ def get_opts():
                         help='the name of recording sensor data')
     parser.add_argument('--saving_file', type=str, default='data.json',
                         help='the name of spliting sensor data')
-    parser.add_argument('--processed_file', type=str, default='data_processed.json',
-                        help='the name of pre-processed sensor data file')
 
-    parser.add_argument('--column', type=str, default='temperature',
-                        help='the name of the column to be predicted')
     parser.add_argument('--filtering_val', type=int, default=20000,
                         help='Number used to delete data whose amount is below this value')
 
@@ -30,10 +23,26 @@ def get_opts():
     parser.add_argument('--lr', type=float, default=0.001,
                         help='learning rate for regression model')
 
-    parser.add_argument('--epochs', type=int, default=1,
+    parser.add_argument('--dp', type=float, default=0.5,
+                        help='Value of Dropout')
+
+    parser.add_argument('--epochs', type=int, default=50,
                         help='Number of training epochs')
+
     parser.add_argument('--data_size', type=int, default=15000,
                         help='Number of dataset size')
+
+    parser.add_argument('--column', type=str, default='temperature',
+                        help='the name of the column to be predicted')
+
+    parser.add_argument('--num_layers', type=int, default=1,
+                        help='Number of LSTM layers')
+
+    parser.add_argument('--seq_length', type=int, default=5,
+                        help='Time steps for input')
+
+    parser.add_argument('--batch_size', type=int, default=32,
+                        help='The size of the input batch')
 
     parser.add_argument('--tw', type=int, default=50,
                         help='training windows')
@@ -41,17 +50,23 @@ def get_opts():
     parser.add_argument('--num_fut', type=int, default=5,
                         help='Number of prediction need')
 
-    parser.add_argument('--rm_model',            type=ast.literal_eval, default=True,
-                        dest='rm_model',
-                        help="True or False flag, Remove model you saved but don't need.")
 
-    parser.add_argument('--data_saving',            type=ast.literal_eval, default=False,
-                        dest='data_saving',
-                        help="True or False flag, Saving data")
+    parser.add_argument('--rm_model',            type = ast.literal_eval, default = True,     
+                    dest = 'rm_model',
+                    help = "True or False flag, Remove model you saved but don't need." )
 
-    parser.add_argument('--debug',            type=ast.literal_eval, default=True,
-                        dest='debug',
-                        help="True or False flag, Debug mode")
+    parser.add_argument('--data_saving',            type = ast.literal_eval, default = False,     
+                    dest = 'data_saving',
+                    help = "True or False flag, Saving data" )
+
+    parser.add_argument('--debug',            type = ast.literal_eval, default = True,     
+                    dest = 'debug',
+                    help = "True or False flag, Debug mode" )
+
+    parser.add_argument('--GPU',            type = ast.literal_eval, default = False,     
+                    dest = 'GPU',
+                    help = "True or False flag, GPU mode" )
+
 
     parser.add_argument('--input_size', type=int, default=1,
                         help='Input size of the regression model')
@@ -62,7 +77,10 @@ def get_opts():
     parser.add_argument('--output_size', type=int, default=1,
                         help='Output size of the regression model')
 
-    # Additional options (add your own hyperparameters here)
+
+    
+
+    ## Additional options (add your own hyperparameters here)
 
     ##
     opts = parser.parse_args()
